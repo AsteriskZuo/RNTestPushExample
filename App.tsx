4ad1761ece02ad06648bfdaf3c8971d06b2d4755
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-07-27 20:57:28 +0800

    chore: update android test

--- App.tsx
+++ App.tsx
@@ -5,114 +5,204 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import * as React from 'react';
+
+import {StyleSheet, View, Text, Pressable, ToastAndroid} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  multiply,
+  ChatPushClient,
+  getPlatform,
+  getDeviceType,
+  type PushType,
+  type ChatPushListener,
+} from 'react-native-push-collection';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [result, setResult] = React.useState<number | undefined>();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const init = React.useCallback(() => {
+    const platform = getPlatform();
+    let pushType: PushType;
+    if (platform === 'ios') {
+      pushType = 'fcm';
+    } else {
+      pushType = (getDeviceType() ?? 'unknown') as PushType;
+    }
+    console.log('test:zuoyu:init:', pushType);
+    ChatPushClient.getInstance()
+      .init({
+        platform: getPlatform(),
+        pushType: pushType as any,
+      })
+      .then(() => {
+        console.log('test:zuoyu:init:addListener');
+        ChatPushClient.getInstance().addListener({
+          onAppBackground: params => {
+            console.log('test:zuoyu:onAppBackground:', params);
+            ToastAndroid.show(
+              'onAppBackground' + JSON.stringify(params),
+              ToastAndroid.SHORT,
+            );
+          },
+          onClickNotification: message => {
+            console.log('test:zuoyu:onClickNotification:', message);
+            ToastAndroid.show(
+              'onClickNotification' + JSON.stringify(message),
+              ToastAndroid.SHORT,
+            );
+          },
+          onError: error => {
+            console.log('test:zuoyu:onError:', error);
+            ToastAndroid.show(
+              'onError' + JSON.stringify(error),
+              ToastAndroid.SHORT,
+            );
+          },
+          onAppForeground: params => {
+            console.log('test:zuoyu:onAppForeground:', params);
+            ToastAndroid.show(
+              'onAppForeground' + JSON.stringify(params),
+              ToastAndroid.SHORT,
+            );
+          },
+          onReceivePushMessage: message => {
+            console.log('test:zuoyu:onReceivePushMessage:', message);
+            ToastAndroid.show(
+              'onReceivePushMessage' + JSON.stringify(message),
+              ToastAndroid.SHORT,
+            );
+          },
+          onReceivePushToken: token => {
+            console.log('test:zuoyu:onReceivePushToken:', token);
+            ToastAndroid.show('onReceivePushToken' + token, ToastAndroid.SHORT);
+          },
+        } as ChatPushListener);
+      })
+      .catch(e => {
+        console.warn('test:zuoyu:init:error:', e);
+        ToastAndroid.show('init error:' + e.toString(), ToastAndroid.SHORT);
+      });
+  }, []);
+
+  const uninit = React.useCallback(() => {
+    ChatPushClient.getInstance().clearListener();
+  }, []);
+
+  const onPrepare = () => {
+    console.log('test:zuoyu:click:onPrepare');
+    ChatPushClient.getInstance()
+      .prepare()
+      .then(() => {
+        console.log('test:zuoyu:prepare');
+        ToastAndroid.show('prepare success', ToastAndroid.SHORT);
+      })
+      .catch(e => {
+        console.warn('test:zuoyu:prepare', e);
+        ToastAndroid.show('prepare error:' + e.toString(), ToastAndroid.SHORT);
+      });
+  };
+
+  const onStartRegistration = () => {
+    console.log('test:zuoyu:click:onStartRegistration');
+    ChatPushClient.getInstance()
+      .registerPush()
+      .then(() => {
+        setTimeout(() => {
+          ChatPushClient.getInstance()
+            .getToken()
+            .then(token => {
+              console.log('test:zuoyu:getToken:', token);
+              ToastAndroid.show('getToken:' + token, ToastAndroid.SHORT);
+            })
+            .catch(e => {
+              console.warn('test:zuoyu:getToken:error:', e);
+              ToastAndroid.show(
+                'getToken error:' + e.toString(),
+                ToastAndroid.SHORT,
+              );
+            });
+        }, 1000);
+      })
+      .catch(e => {
+        console.warn('test:zuoyu:registerPush:error:', e);
+        ToastAndroid.show(
+          'registerPush error:' + e.toString(),
+          ToastAndroid.SHORT,
+        );
+      });
+  };
+  const onStopRegistration = () => {
+    console.log('test:zuoyu:click:onStopRegistration');
+    ChatPushClient.getInstance()
+      .unregisterPush()
+      .then(() => {
+        ChatPushClient.getInstance()
+          .getToken()
+          .then(token => {
+            console.log('test:zuoyu:getToken:', token);
+            ToastAndroid.show('getToken:' + token, ToastAndroid.SHORT);
+          })
+          .catch(e => {
+            console.warn('test:zuoyu:getToken:error:', e);
+            ToastAndroid.show(
+              'getToken error:' + e.toString(),
+              ToastAndroid.SHORT,
+            );
+          });
+      })
+      .catch(e => {
+        console.warn('test:zuoyu:unregisterPush:error:', e);
+        ToastAndroid.show(
+          'unregisterPush error:' + e.toString(),
+          ToastAndroid.SHORT,
+        );
+      });
+  };
+
+  React.useEffect(() => {
+    init();
+    return () => {
+      uninit();
+    };
+  }, [init, uninit]);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+      <Pressable style={styles.button} onPress={onPrepare}>
+        <Text>{'prepare operation'}</Text>
+      </Pressable>
+      <Pressable style={styles.button} onPress={onStartRegistration}>
+        <Text>{'start Registration'}</Text>
+      </Pressable>
+      <Pressable style={styles.button} onPress={onStopRegistration}>
+        <Text>{'stop Registration'}</Text>
+      </Pressable>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    width: 150,
+    height: 40,
+    marginVertical: 20,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
-
-export default App;
--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,15 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    // '/Users/asterisk/tmp/2024.07.26/react-native-push-collection-0.1.0',
+  ],
+  resolver: {
+    nodeModulesPaths: [
+      //   '/Users/asterisk/tmp/2024.07.26/RNTestPushExample/node_modules',
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
