fbf0955ca5c2069947d12357ee84bc700ac0a3f9
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-09-02 10:42:01 +0800

    chore: update

--- metro.config.js
+++ metro.config.js
@@ -8,11 +8,11 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {
   watchFolders: [
-    '/Users/asterisk/tmp/2024.08.08/react-native-push-collection-1.0.0-alpha.2',
+    // '/Users/asterisk/tmp/2024.08.08/react-native-push-collection-1.0.0-alpha.2',
   ],
   resolver: {
     nodeModulesPaths: [
-      '/Users/asterisk/Codes/zuoyu/RNTestPushExample/node_modules',
+      // '/Users/asterisk/Codes/zuoyu/RNTestPushExample/node_modules',
     ],
   },
 };
